ced4bb338a56fa93d697c885c7d715a4f786b50e
elhamsyahrianputra <elhamsyahrianputra220402@gmail.com>
2022-01-30 03:29:58 +0800

    Memperbaiki tampilan mobile

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,25 @@
-const toggle = document.querySelector('.navbar .navbar-item .navbar-toggle button a');
+const themeToggle = document.querySelector('.navbar .navbar-item .navbar-toggle button a');
 const html = document.querySelector('html');
-toggle.addEventListener('click', function() {
-	if (toggle.className === 'bi bi-sun') {
-		toggle.className = 'bi bi-moon';
+themeToggle.addEventListener('click', function() {
+	if (themeToggle.className === 'bi bi-sun') {
+		themeToggle.className = 'bi bi-moon';
 		html.dataset.themeMode = 'dark';
 	} else {
-		toggle.className = 'bi bi-sun';
+		themeToggle.className = 'bi bi-sun';
 		html.dataset.themeMode = 'light';
+	}
+});
+
+
+
+const menuToggle = document.querySelector('.menu-toggle');
+const navLink = document.querySelector('.navbar .navbar-item:nth-child(2)');
+
+menuToggle.addEventListener('click', function() {
+	menuToggle.classList.toggle('menu-toggle-animation');
+	if (menuToggle.classList.contains('menu-toggle-animation')) {
+		navLink.style.right = 0;
+	} else {
+		navLink.style.right = '-60%';
 	}
 });
